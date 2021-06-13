d8914d51f4071361c831d67bb301b9903897251e
unknown <tfisher2@crimson.ua.edu>
2021-06-12 22:51:05 -0500

    server.js changes

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('http://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
 const myApp = express();
-myApp.use(requireHTTPS);
 
 myApp.use(express.static('./dist/personal'));
 
